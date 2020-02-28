785a4a20d9cacd0facf1a94e634152a6a1ebc5d8
geibson <geibson.l@gmail.com>
2020-02-28 01:34:34 -0300

    Report
    Foi adicionado um report ao projeto.

--- ApiTest.py
+++ ApiTest.py
@@ -1,12 +1,24 @@
 import requests
 
-def test_Api():
+
+class test_Api():
+    file=open("D:\\Python\\Sicredi\\RelatorioExecução.txt",mode="a+",encoding="utf-8")
     url = 'http://5b847b30db24a100142dce1b.mockapi.io/api/v1/simulador'    
     resp = requests.get(url)           
-    print(resp.text[0:1])
-    print("    ",resp.text[1:8])
-    print("    ",resp.text[8:42])
-    print("    ",resp.text[42:83])
-    print(resp.text[83:])
+    
+    file.write(resp.text[0:1])
+    file.write("\n")
+    
+    file.write("    " +resp.text[1:8])
+    file.write("\n")
+    
+    file.write("    "+resp.text[8:42])
+    file.write("\n")
+    
+    file.write("    "+resp.text[42:83])
+    file.write("\n")
+    
+    file.write(resp.text[83:])
+    file.write("\n")
 
-  
+    file.close()

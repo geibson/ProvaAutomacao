6f097421c4da4da84f78b5a206d779d681323746
geibson <geibson.l@gmail.com>
2020-02-28 01:33:47 -0300

    Report
    Foi adicionado um arquivo de report, a classe foi alterada para preencher este report

--- Negativo.py
+++ Negativo.py
@@ -5,12 +5,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def Teste_Negativo(v_Perfil,v_Aplicar,v_Investir,v_Tempo,v_medida):
-   
+        file=open("D:\\Python\\Sicredi\\RelatorioExecução.txt",mode="a+",encoding="utf-8")
+        file.write("\n")
+        file.write("Inicio Teste Negativo")
+        file.write("\n")
         driver = webdriver.Firefox()
         #Acessa o site
         driver.get("https://www.sicredi.com.br/html/ferramenta/simulador-investimento-poupanca/")
 
-        print("Valor aplicado: "+v_Aplicar+ " Valor Investido: "+v_Investir+ " por "+v_Tempo +" "+v_medida )
+        file.write("Valor aplicado: "+v_Aplicar+ " Valor Investido: "+v_Investir+ " por "+v_Tempo +"  "+v_medida )
+        file.write("\n")
         #Seleciona o perfil
         driver.find_element_by_css_selector("input[type='radio'][value='"+v_Perfil+"']").click()
         #Encontra o campo Qual o valor que você quer aplicar?*
@@ -40,25 +44,35 @@ def Teste_Negativo(v_Perfil,v_Aplicar,v_Investir,v_Tempo,v_medida):
         #Verifica as mensagens de erro
         if v_Aplicar == "":
                 erro = driver.find_element_by_id('valorAplicar-error')
-                print("Qual o valor que você quer aplicar? = Vazio")
-                print("Mensagem de erro: "+str(erro.text))
+                file.write("Qual o valor que você quer aplicar? = Vazio")
+                file.write("\n")
+                file.write("Mensagem de erro: "+str(erro.text))
+                file.write("\n")
         elif v_Investir == "":
                 erro = driver.find_element_by_id('valorInvestir-error')
-                print("Quanto você quer poupar todo mês? = Vazio")
-                print("Mensagem de erro: "+str(erro.text))
+                file.write("Quanto você quer poupar todo mês? = Vazio")
+                file.write("\n")
+                file.write("Mensagem de erro: "+str(erro.text))
+                file.write("\n")
         elif v_Tempo == "":
                 error = driver.find_element_by_id('tempo-error')
-                print("Por quanto tempo você quer poupar? Vazio ")
-                print("Mensagem de erro: "+str(erro.text))
+                file.write("Campo Obrigatorio")
+                file.write("\n")
+                file.write("Por quanto tempo você quer poupar? Vazio ")
+                file.write("\n")
+                #print("Mensagem de erro: "+str(erro.text))
         else:
                 if driver.find_element_by_css_selector('.valor'):
                     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.btnRefazer')))
                     driver.implicitly_wait(10) # seconds
                     driver.find_element_by_css_selector('.btnRefazer').click()
                 else:
-                    print("Falhou")
-        print("Fim do IF")
-
+                    file.write("\n")
+                    file.write("Falhou")
+        file.write("\n")
+        file.write("Fim do Teste")
+        file.write("\n")
+        file.close()
         #Encerra o teste
         driver.quit()
             
